ae3fcfbb274feea12904a7ec942c05527c24a2db
Sanket Berde <sanket.berde@gmail.com>
2021-07-26 18:13:39 +0530

    misc

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  BLOCKCHAIN_ENDPOINT: "http://localhost:9545",
+  BLOCKCHAIN_CHAIN_ID: 5777,
+  CONTRACT_ADDRESS: "0x537e63609E3a513aB031131a87F9b9CFcD522781",
 };
